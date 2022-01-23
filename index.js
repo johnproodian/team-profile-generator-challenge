b900cd2f8751f479e9bc4c591871335de58b2b2d
Jotas <jproodian721@gmail.com>
2022-01-23 15:39:41 -0600

    aded functions to prompt manager with inquirer and to write the file based on input, template

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const generatePage = require('./src/page-template');
+
+const promptManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'What is your name? '
+        }
+    ])
+};
+
+promptManager()
+    .then(teamData => {
+        const pageHTML = generatePage(teamData);
+        fs.writeFile('./dist/index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+        })
+    });
