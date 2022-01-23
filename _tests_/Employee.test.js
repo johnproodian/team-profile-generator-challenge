6722d3bd0d5d8398ae57c6bcfc4585cb0f3c0dfb
Jotas <jproodian721@gmail.com>
2022-01-23 17:33:37 -0600

    added Employee constructor and passed test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,9 +1,11 @@
 
-// const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee');
 
-// test('creates an Employee object', () => {
-//     const employee = new Employee('Bob', );
+test('creates an Employee object', () => {
+    const employee = new Employee('Rob', '2', 'Bob@fakemail.com');
 
-//     expect(employee.name).toBe('Bob');
+    expect(employee.name).toBe('Rob');
+    expect(employee.id).toBe('2');
+    expect(employee.email).toBe('Bob@fakemail.com');
 
-// })
+})
